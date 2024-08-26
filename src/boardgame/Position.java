9ffb90736d294e2de1b67b2b39ccadf4628dc65d
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-08-26 15:27:56 +0100

    Implementing possible moves of Rock

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -33,6 +33,10 @@ public class Position {
 	}
 
 
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 	@Override
 	public String toString() {
 		return "row " + row + ", " + column;
